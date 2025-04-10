81db6411c46aa892dd56e6b91a1f87c9b9ea6f1d
lyay23 <3050681193@qq.com>
2025-04-10 15:56:49 +0800

    登录校验-Filter校验功能

--- tlias-web-managemen/src/main/java/com/itheima/TliasWebManagemenApplication.java
+++ tlias-web-managemen/src/main/java/com/itheima/TliasWebManagemenApplication.java
@@ -2,10 +2,12 @@ package com.itheima;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * @author 李阳
  */
+@ServletComponentScan // 扫描过滤器
 @SpringBootApplication
 public class TliasWebManagemenApplication {
 
---
+++ tlias-web-managemen/src/main/java/com/itheima/filter/TokenFilter.java
@@ -0,0 +1,68 @@
+package com.itheima.filter;
+
+import com.itheima.util.JwtUtils;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: 李阳
+ * @Date: 2025/04/10/15:03
+ * @Description: 令牌校验Filter的拦截器
+ */
+@Slf4j
+@WebFilter(urlPatterns = {"/*"}) // 拦截所有请求
+public class TokenFilter implements Filter {
+
+    /**
+     *  拦截前端请求，判断请求头中是否包含token
+     */
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        // 1.获取到请求路径
+        // /login
+        String path = request.getRequestURI();
+
+        // 2.判断请求路径是否以/login结尾，判断是不是登录，如果是就放行
+
+        if (path.contains("/login")){
+            log.info("登录请求，放行");
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        // 3.获取请求头中的token
+        String token = request.getHeader("token");
+
+        // 4.判断token是否存在，不存在就返回未登录信息401，存在就放行
+        if (token==null || token.isEmpty()){
+            log.info("令牌为空");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        // 5.如果token存在，就放行，并且将token解析，获取其中的用户信息，校验不通过返回401
+        try {
+            // 解析token
+            JwtUtils.parseToken(token);
+            log.info("令牌解析成功");
+        } catch (Exception e) {
+            log.info("令牌非法");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        }
+
+        // 6.如果token校验通过，将用户放行
+        log.info("令牌校验通过");
+        filterChain.doFilter(request,response);
+    }
+
+}
